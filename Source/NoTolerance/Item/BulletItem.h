2ef178e6c4b719f12ab22b1fb5cb7377595582d7
Mauro Candotti <cmduilio@gmail.com>
2021-05-03 00:23:25 -0300

    test

---
+++ Source/NoTolerance/Item/BulletItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "BulletItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOTOLERANCE_API UBulletItem : public UItem
+{
+	GENERATED_BODY()
+	
+};
